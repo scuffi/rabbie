a1f13a07be746eef82fc74adfe85457ea4fbce7b
Scuffi <45369682+plusgithub@users.noreply.github.com>
2023-04-24 21:30:58 +0100

    Added long description to package

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 version = "0.0.1"
 requirements = ["pika", "multiprocess", "rich", "watchdog"]
 
@@ -10,6 +13,7 @@ setup(
     author_email="iamarchieferguson@gmail.com",
     url="https://github.com/scuffi/rabbie",
     description="A simple, decorator interface for AMQP based message brokers",
+    long_description=long_description,
     license="MIT license",
     packages=find_packages(exclude=["test"]),
     install_requires=requirements,
