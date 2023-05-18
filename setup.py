46a655c957ae05e0e6ec7a0f86174c1a65c5cace
Scuffi <45369682+plusgithub@users.noreply.github.com>
2023-05-18 09:01:17 +0100

    Updated README.md

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = "0.0.7"
+version = "0.0.8"
 requirements = ["pika", "multiprocess", "rich", "watchdog"]
 
 setup(
